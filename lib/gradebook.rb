a75af20d92c70061cf88adcec8141a5cd6f707b4
Dylan Timmons <dylanstimmons@icloud.com>
2023-05-24 10:52:23 -0600

    Add test for students below threshold

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -9,5 +9,23 @@ class Gradebook
   def add_course(course)
     @courses << course
   end
-  
+
+  def list_all_students
+    student_hash = {}
+    @courses.each do |course|
+        student_hash[course] = course.students
+    end
+     student_hash
+    end
+
+  def students_below(threshold)
+    student = []
+    @courses.each do |course|
+        course.students.each do |student|
+            students << student if student.grade < threshold
+        end
+    end
+    students
+  end
+
 end
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -20,15 +20,29 @@ RSpec.describe Gradebook do
 
         it "list_all_students" do
             course = Course.new('Calculus', 2)
-            student1 = Student.new({name: "Morgan", age: 21})
-            student2 = Student.new({name: "Jordan", age: 29})
-
+            student1 = Student.new({name: 'Morgan', age: 21})
+            student2 = Student.new({name: 'Jordan', age: 29})
             course.enroll(student1)
             course.enroll(student2)
-
-            expect(gradebook.list_all_students).to eq({course => [student1, student2]})
+            gradebook.add_course(course)
+            expect(gradebook.list_all_students).to eq({ course => [student1, student2] })
         end
 
+        it "#students_below(threshold)" do
+            course = Course.new('Calculus', 3)
+            student1 = Student.new({name: 'Morgan', age: 21})
+            student2 = Student.new({name: 'Jordan', age: 29})
+            student3 = Student.new({name: 'Gohan', age: 23})
+            course.enroll(student1)
+            course.enroll(student2)
+            course.enroll(student3)
+            gradebook.add_course(course)
+            student1.log_score(80)
+            student2.log_score(70)
+            student3.log_score(90)
+            threshold = 80
+            expect(gradebook.students_below(threshold)).to eq([student2])
+        end
 
     end
 
